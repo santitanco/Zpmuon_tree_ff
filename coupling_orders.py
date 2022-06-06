b7284e0acc0ef3df92d48d0a2ab0ea93f6ceea80
Santiago Tanco <santitanco@gmail.com>
2022-06-06 12:02:24 -0300

    primer prueba

---
+++ coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.1.1 for Linux x86 (64-bit) (June 19, 2020)
+# Date: Fri 30 Oct 2020 00:13:56
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
